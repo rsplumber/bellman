d1cf96256d00a73e12268c57ccc61e14ac96006f
s.kalami <s.kalami@tes.ir>
2023-02-22 09:43:17 +0330

    feature : fake sms provider added.

---
+++ Providers/Sms.Fake/FakeEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using Core;
+using Core.Providers;
+using DotNetCore.CAP;
+
+namespace Sms.Fake;
+
+internal sealed class FakeEventHandler : ICapSubscribe
+{
+    [CapSubscribe("notification_send_fake")]
+    public async Task HandleAsync(ProviderSelectionEvent message)
+    {
+        // var provider = (await _providerRepository.FindAsync(message.Type)).FirstOrDefault();
+
+        Console.WriteLine(message.Content);
+    }
+}
